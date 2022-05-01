1ac8e8e0760334573b4f55e5650b7428a2f56c12
Ritesh <Bisen@Riteshs-MacBook-Air.local>
2022-05-01 22:59:35 +0530

    Added unit test case for subscription

--- src/app/pages/components/comments/comment.component.spec.ts
+++ src/app/pages/components/comments/comment.component.spec.ts
@@ -1,15 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { CommentComponent } from './comment.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FacadeService } from 'src/app/services/facade.service';
-import { IStory } from 'src/app/interfaces/story';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { IStory } from 'src/app/interfaces/story';
+import { of } from 'rxjs/internal/observable/of';
+import { IComment } from 'src/app/interfaces/comment';
 
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
+  let facadeService: FacadeService;
   let fixture: ComponentFixture<CommentComponent>;
   let httpClient: HttpClient;
   const spy = jasmine.createSpyObj('FacadeService', ['getComment']);
@@ -21,11 +24,11 @@ describe('CommentComponent', () => {
     }).compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(inject([FacadeService], s => {
+    facadeService = s;
     fixture = TestBed.createComponent(CommentComponent);
     component = fixture.componentInstance;
-    httpClient = TestBed.get(HttpClient);
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -36,4 +39,14 @@ describe('CommentComponent', () => {
     fixture.detectChanges();
     expect(component.getComment).toHaveBeenCalled();
   });
+
+  it("should call getUsers and return list of users", async(() => {
+    let response: IComment = {id:123};
+    spyOn(facadeService, 'getItem').and.returnValue(of(response))
+    component = fixture.componentInstance;
+    component.getComment('123');
+    fixture.detectChanges();
+    expect(component.story).toEqual(response);
+  }));
+
 });
